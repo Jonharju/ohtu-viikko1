6cb9b1d0b651a28da1f55ccf5e42c2f1d73185df
Jonas Harjunpää <jonas.harjunpaa@helsinki.fi>
2016-03-13 15:03:26 +0200

    testiä muutettu

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -94,7 +94,7 @@ public class VarastoTest {
     public void eiOtaNegatiivista(){
         varasto.lisaaVarastoon(5);
         varasto.otaVarastosta(-2);
-        assertEquals(5, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
    
     @Test
